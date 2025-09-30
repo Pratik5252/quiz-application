f373c092ed0fd83bbbc7dd6e113975be16368ecc
Pratik Yesane <pratiky2000@gmail.com>
2025-09-30 11:56:02 +0530

    Initialize Express server and set up quiz API routes

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import prisma from './prisma/prismaClient.js';
+import quizRoutes from './routes/quiz.js';
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', quizRoutes);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
