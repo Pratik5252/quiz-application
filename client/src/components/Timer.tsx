aad63de573b82f72e674fd6752e930651580636b
Pratik Yesane <pratiky2000@gmail.com>
2025-10-01 20:38:27 +0530

    Add Timer component for countdown functionality in quizzes

---
+++ client/src/components/Timer.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+type TimerProps = {
+    onTimeUp: () => void;
+    time: number;
+    setTime: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Timer = ({ onTimeUp, time, setTime }: TimerProps) => {
+    useEffect(() => {
+        if (time <= 0) {
+            onTimeUp();
+            return;
+        }
+        const timer = setInterval(() => {
+            setTime((prev) => prev - 1);
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [time]);
+
+    const formatTime = (seconds: number) => {
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${mins.toString().padStart(2, '0')}:${secs
+            .toString()
+            .padStart(2, '0')}`;
+    };
+    const lessTime = time <= Math.floor(1 * 60) / 4;
+
+    const timerStyle = lessTime ? 'text-red-600 font-bold' : '';
+    return <div className={timerStyle}>Time Left: {formatTime(time)}</div>;
+};
+
+export default Timer;
