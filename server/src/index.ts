d76a885015faf13ddd4e01fd20b86f69c595d57d
Pratik Yesane <pratiky2000@gmail.com>
2025-10-01 11:22:42 +0530

    Add CORS middleware to allow requests from localhost:5173

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,12 @@ import quizRoutes from './routes/quiz.js';
 const app = express();
 app.use(express.json());
 
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+    })
+);
+
 app.use('/api', quizRoutes);
 
 app.listen(3000, () => {
